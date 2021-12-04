dec1799ffff68bab83aee5fa5a917620f532013d
Dan Ott <danott@users.noreply.github.com>
2021-12-04 11:40:50 -0500

    Implement the example

--- day_03.rb
+++ day_03.rb
@@ -31,6 +31,12 @@ class Day3Test < Minitest::Test
   def test_solution_part_one
     assert_equal 2261546, PowerConsumption.new(real_input).to_i
   end
+
+  def test_example_part_two
+    assert_equal 23, OxygenGeneratorRating.new(example_input).to_i
+    assert_equal 10, CO2ScrubberRating.new(example_input).to_i
+    assert_equal 230, LifeSupportRating.new(example_input).to_i
+  end
 end
 
 PowerConsumption = Struct.new(:measurements) do
@@ -52,6 +58,44 @@ end
 EpsilonRate = Struct.new(:measurements) do
   def value
     measurements.map(&:chars).transpose.map { |column| least_occurring(column, tie: "0") }.join
+  end
+
+  def to_i
+    value.to_i(2)
+  end
+end
+
+LifeSupportRating = Struct.new(:measurements) do
+  def to_i
+    OxygenGeneratorRating.new(measurements).to_i * CO2ScrubberRating.new(measurements).to_i
+  end
+end
+
+OxygenGeneratorRating = Struct.new(:measurements) do
+  def value
+    filtered_measurements = measurements.map(&:chars)
+    filtered_measurements.first.size.times do |index|
+      break if filtered_measurements.size == 1
+      keep = most_occurring(filtered_measurements.transpose[index], tie: "1")
+      filtered_measurements = filtered_measurements.select { |m| m[index] == keep }
+    end
+    filtered_measurements.first.join
+  end
+
+  def to_i
+    value.to_i(2)
+  end
+end
+
+CO2ScrubberRating = Struct.new(:measurements) do
+  def value
+    filtered_measurements = measurements.map(&:chars)
+    filtered_measurements.first.size.times do |index|
+      break if filtered_measurements.size == 1
+      keep = least_occurring(filtered_measurements.transpose[index], tie: "0")
+      filtered_measurements = filtered_measurements.select { |m| m[index] == keep }
+    end
+    filtered_measurements.first.join
   end
 
   def to_i
